f0d99c54c3546a7431fa169f145c80bced2d3c1f
Artur <arturgadyan@mail.ru>
2021-05-08 18:01:53 +0400

    Familiarity with the C # language

---
+++ Lessons/MyConsoleAPP_1/MyConsoleAPP_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyConsoleAPP_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(2 + 2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lessons/MyConsoleAPP_1/WinFormApp_1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormApp_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox1.Text);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is my first programm!", "Info");
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
