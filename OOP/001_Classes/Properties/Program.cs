0148c06b78cb0c5b1022b8fb7c457b250f990129
Artur <arturgadyan123@gmail.com>
2021-12-06 23:34:15 +0400

    Properties

---
+++ OOP/001_Classes/Properties/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+// Using methods of accessing private fields.
+
+namespace Properties
+{
+    class MyClass
+    {
+        private string field = null;
+
+        public void SetField(string value) // mutator  (setter)
+        {
+            field = value;
+        }
+        public string GetField() // accessor  (getter)
+        {
+            return field;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass instance = new MyClass();
+
+            instance.SetField("Hello World!"); // method-mutator
+
+            string @string = instance.GetField(); // method-accessor
+
+            Console.WriteLine(@string);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
