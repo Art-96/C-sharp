fdb75480f991e33b9884d9324d0c55864d1e9509
Artur <arturgadyan123@gmail.com>
2021-12-09 18:34:22 +0400

    Properties_04

---
+++ OOP/001_Classes/Properties_04/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+// Classes. Properties with one access method.
+
+namespace Properties_04
+{
+    class Constants
+    {
+        private double pi = 3.14D;
+        private double e = 2.71D;
+
+        // WriteOnly Property
+        public double Pi
+        {
+            set { pi = value; }
+        }
+
+        // ReadOnly Property
+        public double E
+        {
+            get { return e; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Constants constants = new Constants();
+
+            constants.Pi = 3.14159265D;
+            //Console.WriteLine(constants.Pi); // It is unacceptable.
+
+            //constants.E = 3.71D;             // It is unacceptable.
+            Console.WriteLine("e = {0}", constants.E);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
