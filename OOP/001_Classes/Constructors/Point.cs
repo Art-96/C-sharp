e77c21128f095ca36ec3a2a09d3e34dec42edeb0
Artur <arturgadyan123@gmail.com>
2021-12-14 20:19:48 +0400

    constructor

---
+++ OOP/001_Classes/Constructors/Point.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Point
+    {
+        // Fields.
+        private int x, y;
+
+        // Properties.
+        public int X
+        {
+            get { return x; }
+        }
+        public int Y
+        {
+            get { return y; }
+        }
+
+        // Constructor by default, initializes the default fields.
+        public Point()
+        {
+            Console.WriteLine("Constructor by default!");
+        }
+
+        // Custom constructor, initializes the fields by the values ​​of the specified user.
+        public Point(int x, int y)
+        {
+            Console.WriteLine("Custom constructor!");
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ OOP/001_Classes/Constructors/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Apply the default constructor.
+            Point pointA = new Point();
+            Console.WriteLine("pointA.X = {0}, pointA.Y = {1}", pointA.X, pointA.Y);
+
+            Console.WriteLine(new string('-', 30));
+
+            // Apply a constructor with parameters.
+            Point pointB = new Point(100, 200);
+            Console.WriteLine("pointB.X = {0}, pointB.Y = {1}", pointB.X, pointB.Y);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
