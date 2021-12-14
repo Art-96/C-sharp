e4a80517b7509e4e257383dbc5ac52485f5647b1
Artur <arturgadyan123@gmail.com>
2021-12-14 21:34:23 +0400

    constructor_02

---
+++ OOP/001_Classes/Constructors_02/Point.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// IMPORTANT:
+// If you have created a custom constructor (accepting arguments),
+// the default constructor will not be automatically created, it will have to be created explicitly.
+
+namespace Constructors_02
+{
+    class Point
+    {
+        // Fields.
+        private int x, y;
+
+        // Properties.
+        public int X
+        {
+            get { return x; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+        }
+
+        // The constructor designer initializes the default fields.
+        //public Point()
+        //{
+        //    Console.WriteLine("Default Constructor!");
+        //}
+
+        // The user constructor initializes the fields with the values ​​of the specified user.
+        public Point(int x, int y)
+        {
+            Console.WriteLine("Custom Constructor!");
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ OOP/001_Classes/Constructors_02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// If you have created a custom constructor (accepting arguments),
+// the default constructor will not be automatically created, it will have to be created explicitly.
+
+namespace Constructors_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Apply the constructor designer.
+            //Point pointA = new Point();
+            //Console.WriteLine("pointA.x = {0}, pointA.y = {1}", pointA.X, pointA.Y);
+
+            Console.WriteLine(new string('-', 30));
+
+            // Apply a constructor with parameters.
+            Point pointB = new Point(100, 200);
+            Console.WriteLine("pointB.X = {0}, pointB.Y = {1}", pointB.X, pointB.Y);
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
