7c5fbbfc342a94e1c15726a5580d935b84311eb2
Artur <arturgadyan123@gmail.com>
2021-12-15 21:26:50 +0400

    Classes_03

---
+++ OOP/001_Classes/Classes_03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// Weak reference instantiation. (Anonymous objects)
+
+namespace Classes_03
+{
+    class MyClass
+    {
+        public void Method()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new MyClass().Method();
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
