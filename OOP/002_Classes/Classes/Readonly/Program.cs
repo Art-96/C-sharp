1bdd74cbff895349fda2120835a2b89e23e306bc
Artur <arturgadyan123@gmail.com>
2021-12-22 23:23:52 +0400

    Readonly

---
+++ OOP/002_Classes/Classes/Readonly/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+// readonly - read-only fields.
+
+namespace Classes
+{
+    class Program
+    {
+        // Read-only field (set by constructor only)!
+        public readonly string field = "Hello0";
+
+        // Constructor.
+        public Program()
+        {
+            field = "Read-only field";
+
+            field += "!";
+        }
+        static void Main()
+        {
+            Program program = new Program();
+
+            Console.WriteLine(program.field);
+
+            // Compilation error.
+            // program.field = "An attempt was made to write to a read-only field.";
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
