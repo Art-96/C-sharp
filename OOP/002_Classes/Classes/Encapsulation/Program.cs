0b025a56dfbaec9ec96cfef73d226048ebb5a413
Artur <arturgadyan123@gmail.com>
2021-12-22 22:33:39 +0400

    Encapsulation

---
+++ OOP/002_Classes/Classes/Encapsulation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+// An object cannot hide (encapsulate) anything from another object of the same class.
+
+namespace Classes
+{
+    class MyClass
+    {
+        MyClass my = null;
+
+        private void Method()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public void CallMethod()
+        {
+            my = new MyClass();
+
+            // private method is visible on the instance!
+            my.Method();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass my = new MyClass();
+            my.CallMethod();
+            // my.Method(); // Unacceptable
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
