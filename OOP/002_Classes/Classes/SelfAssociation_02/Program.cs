77e97402bd7bbb160a3c8abfff4d0d315feed1ed
Artur <arturgadyan123@gmail.com>
2021-12-23 15:16:24 +0400

    Self Association_02

---
+++ OOP/002_Classes/Classes/SelfAssociation_02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+// self-association
+
+namespace Classes
+{
+    class MyClass
+    {
+        MyClass instance;
+
+        public MyClass()  // StackOverflowException
+        {
+            Console.WriteLine("Constructor MyClass");
+
+            // Self-association, after the first instantiation attempt, leads to cyclic instantiation.
+
+            instance = new MyClass();
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            // Attempting to instantiate the MyClass
+
+            MyClass instance = new MyClass();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
