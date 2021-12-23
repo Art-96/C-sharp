7e7a7de3d6dfc4461f0f809f37e18cce81652710
Artur <arturgadyan123@gmail.com>
2021-12-23 15:28:58 +0400

    Association

---
+++ OOP/002_Classes/Classes/Association/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+// Using the technique of factory methods.
+
+namespace Classes
+{
+    class Product
+    {
+        public Product()
+        {
+            Console.WriteLine("An instance of the Product class has been created");
+        }
+    }
+
+    class Factory
+    {
+        public Product FactoryMethod()
+        {
+            return new Product();
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Factory factory = new Factory();
+
+            Product product = factory.FactoryMethod();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
