0e7a18d98921a9b3e8bad0af64ea80aaf30e0574
Artur <arturgadyan123@gmail.com>
2021-12-23 12:50:52 +0400

    Self Association

---
+++ OOP/002_Classes/Classes/SelfAssociation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+// self-association
+
+namespace Classes
+{
+    class Program
+    {
+        public void Method()
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static void Main()
+        {
+            Program program = new Program();
+
+            program.Method();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
