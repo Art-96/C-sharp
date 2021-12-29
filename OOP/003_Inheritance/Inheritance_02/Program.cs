09e7df4f74fc6948decb2a5d0e84d05d3d7848a6
Artur <arturgadyan123@gmail.com>
2021-12-29 14:35:27 +0400

    Inheritance_02

---
+++ OOP/003_Inheritance/Inheritance_02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+// Casting to base type.
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main()
+        {
+            DerivedClass instance = new DerivedClass();
+
+            instance.field1 = 1;
+            instance.field2 = 2;
+            instance.field3 = 3;
+            instance.field4 = 4;
+            instance.field5 = 5;
+
+            // Casting an instance of the DerivedClass class to the base type BaseClass.
+            BaseClass newinstance = (BaseClass)instance; // Upcast
+
+            Console.WriteLine(newinstance.field1);
+            Console.WriteLine(newinstance.field2);
+            Console.WriteLine(newinstance.field3);
+
+            // Not available.
+            //Console.WriteLine(newInstance.field4);
+            //Console.WriteLine(newInstance.field5);
+
+
+            // Examination.
+            Console.WriteLine("instance Id    {0}", instance.GetHashCode());
+            Console.WriteLine("newinstance Id {0}", newinstance.GetHashCode());
+
+        }
+    }
+}
