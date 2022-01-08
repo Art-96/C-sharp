1ac13d98a7fec33651892460c8fd38327e35268f
Artur <arturgadyan123@gmail.com>
2022-01-08 12:04:24 +0400

    Inheritance_03

---
+++ OOP/003_Inheritance/Inheritance_03/DerivedClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class DerivedClass : BaseClass
+    {
+        public void Method()
+        {
+            Console.WriteLine("Method from DerivedClass");
+        }
+    }
+}
---
+++ OOP/003_Inheritance/Inheritance_03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main()
+        {
+            DerivedClass instance = new DerivedClass();
+            instance.Method();
+
+            // UpCast
+            BaseClass instanceup = instance;
+            instanceup.Method();
+
+            // DownCast
+            DerivedClass instancedown = (DerivedClass)instanceup;
+            instancedown.Method();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
