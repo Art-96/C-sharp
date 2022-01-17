9abfc2d40df267b22d18bf8e66322c8cada526c5
Artur <arturgadyan123@gmail.com>
2022-01-17 12:49:18 +0400

    base.Method

---
+++ OOP/003_Inheritance/base.Method/DerivedClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class DerivedClass : BaseClass
+    {
+        // Overriding a base class method.
+
+        public override void Method()
+        {
+            // Calling a base class method.
+            base.Method();
+
+            Console.WriteLine("Method from DerivedClass");
+        }
+    }
+}
