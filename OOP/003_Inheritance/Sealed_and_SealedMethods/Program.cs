17279e7e742099208d30591aa64042279d483d99
Artur <arturgadyan123@gmail.com>
2022-01-17 20:06:17 +0400

    Sealed_and_SealedMethods

---
+++ OOP/003_Inheritance/Sealed_and_SealedMethods/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Inheritance
+{
+    partial class ClassA
+    {
+        public virtual void Method1() { Console.WriteLine("ClassA.Method1"); }
+        public virtual void Method2() { Console.WriteLine("ClassA.Method2"); }
+    }
+
+    partial class ClassB : ClassA
+    {
+        sealed public override void Method1() { Console.WriteLine("ClassB.Method1"); }
+        public override void Method2() { Console.WriteLine("ClassB.Method2"); }
+    }
+
+    class ClassC : ClassB
+    {
+        // Attempting to override Method1 results in compiler error: CS0239.
+        // public override void Method1() { Console.WriteLine("ClassC.Method1"); }
+
+        // Overriding Method2 is allowed.
+        public override void Method2() { Console.WriteLine("ClassC.Method2"); }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            SealedClass instance = new SealedClass();
+            instance.x = 100;
+            instance.y = 200;
+            Console.WriteLine("x = {0}, y = {1}", instance.x, instance.y);
+
+
+            ClassA instanceA = new ClassA();
+            instanceA.Method1();
+            instanceA.Method2();
+
+            Console.WriteLine(new string('-', 15));
+
+            ClassB instanceB = new ClassB();
+            instanceB.Method1();
+            instanceB.Method2();
+
+            Console.WriteLine(new string('-', 15));
+
+            ClassC instanceC = new ClassC();
+            instanceC.Method1();
+            instanceC.Method2();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP/003_Inheritance/Sealed_and_SealedMethods/SealedClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+// Sealed classes.
+
+namespace Inheritance
+{
+    sealed class SealedClass
+    {
+        public int x;
+        public int y;
+    }
+
+    // Attempt to inherit from SealedClass results in a compiler error.
+
+    class DerivedClass //: SealedClass   // Error
+    {
+    }
+}
