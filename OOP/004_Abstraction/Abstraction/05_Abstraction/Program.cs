5cabde94fb9305620668ba71f5d041bafcee493d
Artur <arturgadyan123@gmail.com>
2022-01-18 22:36:04 +0400

    05_Abstraction

---
+++ OOP/004_Abstraction/Abstraction/05_Abstraction/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Abstraction
+{
+    abstract class AbstractClass
+    {
+        public AbstractClass()
+        {
+            Console.WriteLine("1 AbstractClass()");
+
+            // The implementation of the method from the derived class is called - ConcreteClass.
+            this.AbstractMethod();
+
+            Console.WriteLine("2 AbstractClass()");
+        }
+
+        public abstract void AbstractMethod();
+    }
+
+    class ConcreteClass : AbstractClass
+    {
+        string s = "FIRST";
+
+        public ConcreteClass()
+        {
+            Console.WriteLine("3 ConcreteClass()");
+            s = "SECOND";
+        }
+
+        public override void AbstractMethod()
+        {
+            Console.WriteLine("Method Implementation AbstractMethod() in ConcreteClass  {0}", s);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            AbstractClass instance = new ConcreteClass();
+
+            Console.WriteLine(new string('-', 55));
+
+            instance.AbstractMethod();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
