85349d05388b4a1230041f73f91d9ec5c96ff18e
Artur <arturgadyan123@gmail.com>
2022-01-18 13:03:00 +0400

    01_Abstraction

---
+++ OOP/004_Abstraction/Abstraction/01_Abstraction/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+// Abstract class.
+
+namespace Abstraction
+{
+    // Abstract class.
+    abstract class AbstractClass
+    {
+        public abstract void Method();
+    }
+
+    // Concrete class.
+    class ConcreteClass : AbstractClass
+    {
+        public override void Method()
+        {
+            Console.WriteLine("Implementation");
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            AbstractClass instance = new ConcreteClass();
+
+            instance.Method();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
