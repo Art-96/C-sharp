23f291a9ad94d699a1d97c08a64e7404284c739d
Artur <arturgadyan123@gmail.com>
2022-01-18 13:16:40 +0400

    02_Abstraction

---
+++ OOP/004_Abstraction/Abstraction/02_Abstraction/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+// An abstract class can be inherited from a concrete class.
+
+namespace Abstraction
+{
+    class ConcreteClassA
+    {
+        public void Operation()
+        {
+            Console.WriteLine("ConcreteClassA.Operation");
+        }
+    }
+
+    abstract class AbstractClass : ConcreteClassA
+    {
+        public abstract void Method();
+    }
+
+    class ConcreteClassB : AbstractClass
+    {
+        public override void Method()
+        {
+            Console.WriteLine("ConcreteClassB.Method");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            AbstractClass instance = new ConcreteClassB();
+
+            instance.Method();
+            instance.Operation();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
