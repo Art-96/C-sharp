de41d9d8507ad0100457cd1fdd8ca971fa87fe7c
Artur <arturgadyan123@gmail.com>
2022-01-18 22:04:05 +0400

    04_Abstraction

---
+++ OOP/004_Abstraction/Abstraction/04_Abstraction/DerivedClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Abstraction
+{
+    class ConcreteDerivedClass : AbstractBaseClass
+    {
+        public override void VirtualMethod()
+        {
+            Console.WriteLine("DerivedClass.VirtualMethod();");
+        }
+
+        public override void AbstractMethod()
+        {
+            Console.WriteLine("DerivedClass.AbstractMethod();");
+        }
+    }
+}
