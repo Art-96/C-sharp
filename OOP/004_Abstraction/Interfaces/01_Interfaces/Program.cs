8a5241bc89735c93eb136af84c28f04560397535
Artur <arturgadyan123@gmail.com>
2022-01-19 14:26:58 +0400

    01_Interfaces

---
+++ OOP/004_Abstraction/Interfaces/01_Interfaces/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Interfaces
+{
+    interface IInterface
+    {
+        void Method();
+    }
+
+    class MyClass : IInterface
+    {
+        public void Method()
+        {
+            Console.WriteLine("Method - implementation of the Interface.");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            MyClass instance = new MyClass();
+
+            instance.Method();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
