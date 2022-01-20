987dd6abfd703fd6270489584445b2c8bc84591e
Artur <arturgadyan123@gmail.com>
2022-01-20 16:04:56 +0400

    03_Interfaces

---
+++ OOP/004_Abstraction/Interfaces/03_Interfaces/DerivedClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class DerivedClass : Interface1, Interface2
+    {
+        // On the 13th line, we implement a method named Method from the base interface Interface1
+        // When implementing a method, we use the technique of explicitly specifying the interface name
+        // in the name of the method to which this method belongs.
+
+        // By default, methods of the same name are private, but explicitly
+        // specifying access modifiers is not allowed.
+        void Interface1.Method()
+        {
+            Console.WriteLine("Implementation of the Method() method from Interface1");
+        }
+
+        void Interface2.Method()
+        {
+            Console.WriteLine("Implementation of the Method() method from Interface2");
+        }
+    }
+}
---
+++ OOP/004_Abstraction/Interfaces/03_Interfaces/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// Explicit specification of the interface name in the method name.
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main()
+        {
+            DerivedClass instance = new DerivedClass();
+
+            //instance. -- // On the instance, we do not see the methods of the interfaces.
+
+            // Let's cast an instance of the class DerivedClass - instance, to the base interface type Interface1
+
+            Interface1 instance1 = instance as Interface1;
+            instance1.Method();
+
+            Interface2 instance2 = instance as Interface2;
+            instance2.Method();
+
+            //Delay.
+            Console.ReadKey();
+
+        }
+    }
+}
