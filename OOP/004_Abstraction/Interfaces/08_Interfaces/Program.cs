12ef818f347d1b11c4e75ae82e51f24a12e4b2c2
Artur <arturgadyan123@gmail.com>
2022-01-20 20:10:21 +0400

    Inheritance of abstract classes from interfaces.

---
+++ OOP/004_Abstraction/Interfaces/08_Interfaces/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Interfaces
+{
+    interface Interface
+    {
+        void Method();
+    }
+
+    abstract class AbstractClass : Interface
+    {
+        // Replacing an abstract method from an interface in an abstract class is mandatory.
+        public abstract void Method();
+    }
+
+    class ConcreteClass : AbstractClass
+    {
+        // The implementation of an abstract method from an abstract class in a concrete class is required.
+        public override void Method()
+        {
+            Console.WriteLine("Method - implementation of an interface in an abstract class.");
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            ConcreteClass instance = new ConcreteClass();
+            instance.Method();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
