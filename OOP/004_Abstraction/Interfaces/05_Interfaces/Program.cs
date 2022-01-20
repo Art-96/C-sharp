b11fbea4497914f58976ad785ffd01d66ceeb66e
Artur <arturgadyan123@gmail.com>
2022-01-20 17:09:54 +0400

    Inheriting an interface from an interface.

---
+++ OOP/004_Abstraction/Interfaces/05_Interfaces/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Interfaces
+{
+    interface IInterface1
+    {
+        void Method1();
+    }
+
+    interface IInterface2 : IInterface1
+    {
+        void Method2();
+    }
+
+    class ConcreteClass : IInterface2
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Method1 - implementation of the IInterface1 interface.");
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine("Method2 - implementation of the IInterface2 interface.");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            ConcreteClass instance = new ConcreteClass();
+            instance.Method1();
+            instance.Method2();
+
+            IInterface1 instance1 = instance as IInterface1;
+            instance1.Method1();
+
+            IInterface2 instance2 = instance as IInterface2;
+            instance2.Method1();
+            instance2.Method2();
+
+            //Delay.
+            Console.ReadKey();
+        }
+    }
+}
