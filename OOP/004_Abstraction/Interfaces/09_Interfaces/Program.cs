3584ef01520f87198227389574451c6da3d81499
Artur <arturgadyan123@gmail.com>
2022-01-20 20:16:23 +0400

    Inheritance from a class and an interface that have the same member signatures.

---
+++ OOP/004_Abstraction/Interfaces/09_Interfaces/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Interfaces
+{
+    interface IInterface
+    {
+        void Method();
+    }
+
+    class BaseClass
+    {
+        public void Method()
+        {
+            Console.WriteLine("BaseClass.Method()");
+        }
+    }
+
+    class DerivedClass : BaseClass, IInterface
+    {
+        // Interface implementation is optional, because,
+        // method signatures in the class and interface are the same.
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            DerivedClass instance = new DerivedClass();
+            instance.Method();
+
+            IInterface instance1 = instance as IInterface;
+            instance1.Method();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
