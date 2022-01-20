c35827b6119faf6ec3db970c0e92ff3edf7b95d3
Artur <arturgadyan123@gmail.com>
2022-01-20 14:59:11 +0400

    02_Interfaces

---
+++ OOP/004_Abstraction/Interfaces/02_Interfaces/DerivedClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class DerivedClass : Interface1, Interface2
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Implementation of the Method1() method from Interface1");
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine("Implementation of the Method2() method from Interface2");
+        }
+    }
+}
---
+++ OOP/004_Abstraction/Interfaces/02_Interfaces/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main()
+        {
+            Interface1 instance1 = new DerivedClass();
+            Interface2 instance2 = new DerivedClass();
+
+            instance1.Method1();
+
+            instance2.Method2();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
