7ef66094711a038eb48a4bb114afef8a3033025c
Artur <arturgadyan123@gmail.com>
2022-01-20 19:57:05 +0400

    Combining implementations of abstract methods with the same name.

---
+++ OOP/004_Abstraction/Interfaces/07_Interfaces/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Interfaces
+{
+    interface IInterface1
+    {
+        void Method();
+    }
+
+    interface IInterface2
+    {
+        void Method();
+    }
+
+    class ConcreteClass : IInterface1, IInterface2
+    {
+        public void Method()
+        {
+            Console.WriteLine("Method1 - implementation of the IInterface (1-2) interface.");
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            ConcreteClass instance = new ConcreteClass();
+            //instance.Method();
+
+            IInterface1 instance1 = instance as IInterface1;
+            instance1.Method();
+
+            IInterface2 instance2 = instance as IInterface2;
+            instance2.Method();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
