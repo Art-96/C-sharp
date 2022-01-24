4d88bed40c2c308ff4bdcd5095bc5afcdba82917
Artur <arturgadyan123@gmail.com>
2022-01-24 20:33:55 +0400

    Implicitly Typed Arrays.

---
+++ OOP/005_Arrays(Indexers)/Arrays/06_VarArrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main()
+        {
+            var array1 = new[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine(array1.GetType());
+
+            var array2 = new[] { 3.1415, 1, 6 };
+            Console.WriteLine(array2.GetType());
+
+            // Doesn't compile. (Incompatible types)
+            // var array3 = new [] { 1, "string" };
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
