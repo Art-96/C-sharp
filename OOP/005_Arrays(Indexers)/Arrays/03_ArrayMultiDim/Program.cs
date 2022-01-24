a9e27728b91d3f06218e59ba7bf1c6648c127be4
Artur <arturgadyan123@gmail.com>
2022-01-24 18:30:54 +0400

    Array MultiDim

---
+++ OOP/005_Arrays(Indexers)/Arrays/03_ArrayMultiDim/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[,,] threeDim = {
+                                  {{1, 2},{ 3, 4}},
+                                  {{5, 6}, {7, 8}}
+                                };
+
+            // Get the number of subarrays in the Array - GetLength(0). - 2 subarrays.
+            for (int i = 0; i < threeDim.GetLength(0); ++i)
+            {
+                // Get the number of elements in the Subarray - GetLength(1). - 4 elements in each subarray.
+                for (int j = 0; j < threeDim.GetLength(1); ++j)
+                {
+                    for (int k = 0; k < threeDim.GetLength(2); k++)
+                    {
+                        Console.Write(threeDim[i, j, k] + ", ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine(threeDim.Length);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
