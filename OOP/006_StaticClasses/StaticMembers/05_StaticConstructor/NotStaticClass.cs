dfc9d3e5890e891e2b2ccf196be511aedb1bc58e
Artur <arturgadyan123@gmail.com>
2022-01-25 15:45:29 +0400

    05_StaticConstructor

---
+++ OOP/006_StaticClasses/StaticMembers/05_StaticConstructor/NotStaticClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace StaticMembers
+{
+    class NotStaticClass
+    {
+        // If the class contains static fields, a static constructor must be provided that initializes
+        // those fields when the class is loaded.
+
+        private static int field;
+
+        public static int Property
+        {
+            get { return field; }
+            set { field = value; }
+        }
+
+        static NotStaticClass()
+        {
+            Console.WriteLine("Static constructor - NotStaticClass()");
+            field = 1;
+        }
+
+        // Static methods can be overloaded.
+        public static void Method()
+        {
+            Console.WriteLine("Static method, non-static NotStaticClass");
+        }
+        public static void Method(int s)
+        {
+            Console.WriteLine("Overloaded static method, non-static NotStaticClass" + s);
+        }
+
+        public void NotStaticMethod()
+        {
+            Console.WriteLine(field);
+        }
+    }
+}
---
+++ OOP/006_StaticClasses/StaticMembers/05_StaticConstructor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StaticMembers
+{
+    class Program
+    {
+        static void Main()
+        {
+            NotStaticClass instance = new NotStaticClass();
+            instance.NotStaticMethod();
+
+            NotStaticClass.Property = 2;
+            Console.WriteLine(NotStaticClass.Property);
+
+            NotStaticClass.Method();
+            NotStaticClass.Method(3);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
