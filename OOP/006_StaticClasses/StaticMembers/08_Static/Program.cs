284be576ee7a578f480fac194b80a7f206536b31
Artur <arturgadyan123@gmail.com>
2022-01-25 16:25:43 +0400

    08_Static

---
+++ OOP/006_StaticClasses/StaticMembers/08_Static/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+// Static members cannot be virtual, overridden, or abstract.
+
+// Static members support substitution.
+
+namespace StaticMembers
+{
+    abstract class BaseClass
+    {
+        //public static virtual void StaticMethod()
+        //{
+        //    Console.WriteLine("BaseClass.StaticMethod");
+        //}
+
+        public static void StaticMethod()
+        {
+            Console.WriteLine("BaseClass.StaticMethod");
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        //public static override void StaticMethod()
+        //{
+        //    Console.WriteLine("DerivedClass.StaticMethod");
+        //}
+
+        public static void StaticMethod()
+        {
+            Console.WriteLine("DerivedClass.StaticMethod");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            BaseClass.StaticMethod();
+            DerivedClass.StaticMethod();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
