fc83ca36beb312c43dbd57e734e643c0500cb37f
Artur <arturgadyan123@gmail.com>
2022-01-25 15:01:13 +0400

    Static members in non-static classes.

---
+++ OOP/006_StaticClasses/StaticMembers/01_Static/NotStaticClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StaticMembers
+{
+    class NotStaticClass
+    {
+        private int Id;
+        public static int field;
+
+        public NotStaticClass(int id)
+        {
+            this.Id = id;
+        }
+
+        public void Method()
+        {
+            Console.WriteLine("Instance{0}.field = {1}", Id, field);
+        }
+
+        public static void StaticMethod()
+        {
+            // Static methods cannot access non-static fields.
+            //Console.WriteLine("Instance.Id = {0}", Id);
+
+            Console.WriteLine("Static methods cannot access non-static fields.");
+        }
+    }
+}
---
+++ OOP/006_StaticClasses/StaticMembers/01_Static/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StaticMembers
+{
+    class Program
+    {
+        static void Main()
+        {
+            NotStaticClass instance1 = new NotStaticClass(1);
+            NotStaticClass instance2 = new NotStaticClass(2);
+
+            instance1.Method();
+            instance2.Method();
+
+            // accessing a static field - field
+            NotStaticClass.field = 1;
+
+            instance1.Method();
+            instance2.Method();
+
+            NotStaticClass.StaticMethod();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
