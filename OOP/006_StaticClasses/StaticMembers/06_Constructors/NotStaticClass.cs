5724c2a1a782b608f62a324fbc0bbd07ef281b24
Artur <arturgadyan123@gmail.com>
2022-01-25 16:14:25 +0400

    06_Constructors

---
+++ OOP/006_StaticClasses/StaticMembers/06_Constructors/NotStaticClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StaticMembers
+{
+    class NotStaticClass
+    {
+        public NotStaticClass()
+        {
+            Console.WriteLine("Constructor.");
+        }
+
+        static NotStaticClass()
+        {
+            Console.WriteLine("Static Constructor.");
+        }
+
+        public static void StaticMethod()
+        {
+            Console.WriteLine("StaticMethod()");
+        }
+
+        public void NotStaticMethod()
+        {
+            Console.WriteLine("NotStaticMethod()");
+        }
+    }
+}
---
+++ OOP/006_StaticClasses/StaticMembers/06_Constructors/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StaticMembers
+{
+    class Program
+    {
+        static void Main()
+        {
+            // 1 option. (Only the static constructor is called.)
+            NotStaticClass.StaticMethod();
+
+            // 2 option. (Both Constructors are called.)
+            //new NotStaticClass().NotStaticMethod();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
