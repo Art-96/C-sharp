b5b0b99f58515ce050bf12797a7146c27a8210c2
Artur <arturgadyan123@gmail.com>
2022-01-25 15:08:23 +0400

    02_Constant

---
+++ OOP/006_StaticClasses/StaticMembers/02_Constant/NotStaticClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticMembers
+{
+    class NotStaticClass
+    {
+        // Constants cannot be static.
+        // public static const float e = 2.71828182845904523536f;
+
+        // A field cannot be declared as static const, a const field by its behavior is already static.
+        // A const field refers to a type, not to instances of the type.
+        // Therefore, const fields can be accessed using the same ClassName.MemberName notation,
+        // as in used for static fields.
+
+        public const float e = 2.71828182845904523536f;
+    }
+}
---
+++ OOP/006_StaticClasses/StaticMembers/02_Constant/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StaticMembers
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("e = {0}", NotStaticClass.e);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
