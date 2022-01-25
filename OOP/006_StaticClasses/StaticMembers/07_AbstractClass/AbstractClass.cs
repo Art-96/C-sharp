d9971246ce679a3e55170c31487277c1df87a5b3
Artur <arturgadyan123@gmail.com>
2022-01-25 16:21:38 +0400

    Static members in abstract classes.

---
+++ OOP/006_StaticClasses/StaticMembers/07_AbstractClass/AbstractClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StaticMembers
+{
+    abstract class AbstractClass
+    {
+        public static AbstractClass CreateObject()
+        {
+            return new ConcreteClass();
+        }
+
+        public abstract void Method();
+    }
+}
---
+++ OOP/006_StaticClasses/StaticMembers/07_AbstractClass/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StaticMembers
+{
+    class Program
+    {
+        static void Main()
+        {
+            AbstractClass instance = AbstractClass.CreateObject();
+            instance.Method();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
