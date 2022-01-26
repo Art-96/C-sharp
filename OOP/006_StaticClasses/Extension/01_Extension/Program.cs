2add914c609ba351053b8514539bca89f86193fb
Artur <arturgadyan123@gmail.com>
2022-01-26 11:29:11 +0400

    Extension methods.

---
+++ OOP/006_StaticClasses/Extension/01_Extension/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+// Extension methods can only be static and can only be created in static classes.
+
+namespace Extension
+{
+    static class ExtensionClass
+    {
+        // this - tells the compiler that this method is an extension method (Extension)!
+        public static void ExtensionMethod(this string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            string text = "Test string";
+
+            ExtensionClass.ExtensionMethod(text);
+
+            text.ExtensionMethod();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP/006_StaticClasses/Extension/02_Extension/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+// The extension argument must always be only one and come first in the argument list.
+
+namespace Extension
+{
+    static class ExtensionClass
+    {
+        public static void ExtensionMethod(this string value1, string value2)
+        {
+            Console.WriteLine(value1 + value2);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            string text = "Hello ";
+
+            text.ExtensionMethod("World!");
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
