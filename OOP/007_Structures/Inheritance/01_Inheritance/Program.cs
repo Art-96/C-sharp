50f9bad774b7310e58bfc730854c99c7ba7deb0a
Artur <arturgadyan123@gmail.com>
2022-01-26 20:18:48 +0400

    Structures. Inheritance.

---
+++ OOP/007_Structures/Inheritance/01_Inheritance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+// Struct inheritance is only allowed from interfaces.
+// Inheritance of structures from classes and structures is prohibited.
+// All structures are implicitly inherited from the abstract class - ValueType
+
+namespace Inheritance
+{
+    interface IInterface
+    {
+        void Method();
+    }
+
+    struct MyStruct : IInterface
+    {
+        public void Method()
+        {
+            Console.WriteLine("Method");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            MyStruct instance;
+
+            instance.Method();
+
+            MyStruct instance2 = new MyStruct();
+
+            ValueType valueType = instance2 as ValueType;
+
+            Console.WriteLine("instance  = {0}", instance2.GetHashCode());
+            Console.WriteLine("valueType = {0}", valueType.GetHashCode());
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP/007_Structures/Inheritance/02_Inheritance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+// Structures cannot be inherited.
+
+namespace Inheritance
+{
+    struct MyStruct
+    {
+        // Structs cannot have protected members.
+        // protected int field;
+    }
+
+    class MyClass //: MyStruct
+    {
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Structures cannot be inherited.");
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
