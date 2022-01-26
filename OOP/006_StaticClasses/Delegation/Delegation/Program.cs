f8dd6ef72a8655373f3fc7c889cf010b1b9b5c8d
Artur <arturgadyan123@gmail.com>
2022-01-26 12:39:13 +0400

    Delegation technique.

---
+++ OOP/006_StaticClasses/Delegation/Delegation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Delegation
+{
+    class A
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("Action");
+        }
+
+    }
+
+    class B
+    {
+        public void DoSomething()
+        {
+            A instance = new A();
+
+            instance.DoSomething();
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            B instance = new B();
+
+            instance.DoSomething();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
