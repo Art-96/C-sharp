0d5a67861b895f92ef5616bb964d0a5f3ea6a736
Artur <arturgadyan123@gmail.com>
2022-02-03 17:53:23 +0400

    Generics Constraints.

---
+++ OOP/011_Generics(Constraints)/Dictionary/Dictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+
+            dictionary.Add(0, "zero");
+            dictionary.Add(1, "one");
+            dictionary.Add(2, "two");
+            dictionary.Add(3, "three");
+            dictionary.Add(4, "four");
+
+            for (int i = 0; i < dictionary.Count; i++)
+            {
+                Console.WriteLine(dictionary[i]);
+            }
+        }
+    }
+}
---
+++ OOP/011_Generics(Constraints)/List/ArrayList_ListGen/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ArrayList_ListGen
+{
+    class Program
+    {
+        static void Main()
+        {
+            ArrayList arrayList = new ArrayList();
+
+            // Boxing.
+            arrayList.Add(1);
+            arrayList.Add((object)2);
+
+            // Unboxing
+            int i1 = (int)arrayList[0];
+
+            for (int i = 0; i < arrayList.Count; i++)
+            {
+                Console.WriteLine((int)arrayList[i]);
+            }
+
+            Console.WriteLine(new string('-', 5));
+
+            List<int> list = new List<int>();
+
+            list.Add(3);
+            list.Add(5);
+
+            int i3 = list[0];
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
+    }
+}
