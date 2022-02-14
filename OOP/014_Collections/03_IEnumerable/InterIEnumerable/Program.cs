f36b17bfada73b95d1f0d52340ded6540225a882
Artur <arturgadyan123@gmail.com>
2022-02-14 21:59:35 +0400

    03_IEnumerable

---
+++ OOP/014_Collections/03_IEnumerable/InterIEnumerable/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+
+namespace InterIEnumerable
+{
+    class Program
+    {
+        static void Main()
+        {
+            UserCollection myCollection = new UserCollection();
+
+            foreach (Element element in myCollection)
+            {
+                Console.WriteLine("Name: {0}  Field1: {1} Field2: {2}", element.Name, element.Field1, element.Field2);
+            }
+
+            Console.Write(new string('-', 29) + "\n");
+
+            foreach (Element element in myCollection)
+            {
+                Console.WriteLine("Name: {0}  Field1: {1} Field2: {2}", element.Name, element.Field1, element.Field2);
+            }
+
+            Console.Write(new string('-', 29) + "\n");
+
+
+            // --------------------------------------------------------------------------------------------------------------------
+
+            UserCollection myElementsCollection = new UserCollection();
+
+            IEnumerable enumerable = myElementsCollection as IEnumerable;
+
+            IEnumerator enumerator = enumerable.GetEnumerator();
+
+
+            while (enumerator.MoveNext())
+            {
+                Element element = enumerator.Current as Element;
+
+                Console.WriteLine("Name: {0}  Field1: {1} Field2: {2}", element.Name, element.Field1, element.Field2);
+            }
+        }
+    }
+}
---
+++ OOP/014_Collections/03_IEnumerable/InterIEnumerable/UserCollection/UserCollection.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterIEnumerable
+{
+    class UserCollection : IEnumerable
+    {
+        public Element[] elementsArray = null;
+
+        public UserCollection()
+        {
+            elementsArray = new Element[4];
+            elementsArray[0] = new Element("A", 1, 10);
+            elementsArray[1] = new Element("B", 2, 20);
+            elementsArray[2] = new Element("C", 3, 30);
+            elementsArray[3] = new Element("D", 4, 40);
+        }
+
+        int position = -1;
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+
+        // -------------------------------------------------------------------------------------------------------------------------
+
+        public IEnumerator GetEnumerator()
+        {
+            while (true)
+            {
+                if (position < elementsArray.Length - 1)
+                {
+                    position++;
+                    yield return elementsArray[position];
+                }
+                else
+                {
+                    Reset();
+                    yield break;     
+                }
+            }
+        }
+    }
+}
