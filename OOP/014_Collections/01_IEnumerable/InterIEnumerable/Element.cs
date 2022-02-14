03a6ec651c8bb7ce113614cf993732ccaaa17108
Artur <arturgadyan123@gmail.com>
2022-02-14 21:29:25 +0400

    01_IEnumerable

---
+++ OOP/014_Collections/01_IEnumerable/InterIEnumerable/Element.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterIEnumerable
+{
+    class Element
+    {
+        private string name;
+        private int field1;
+        private int field2;
+
+        public Element(string s, int a, int b)
+        {
+            this.name = s;
+            this.field1 = a;
+            this.field2 = b;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Field1
+        {
+            get { return this.field1; }
+            set { this.field1 = value; }
+        }
+
+        public int Field2
+        {
+            get { return this.field2; }
+            set { this.field2 = value; }
+        }
+    }
+}
---
+++ OOP/014_Collections/01_IEnumerable/InterIEnumerable/UserCollection.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterIEnumerable
+{
+    class UserCollection : IEnumerable, IEnumerator
+    {
+        public Element[] elementsArray = null;
+
+        public UserCollection()
+        {
+            elementsArray = new Element[4];
+            elementsArray[0] = new Element("A", 1, 10);
+            elementsArray[1] = new Element("B", 2, 20);
+            elementsArray[2] = new Element("C", 3, 30);
+            elementsArray[3] = new Element("D", 4, 40);
+        }
+
+        int posicion = -1;
+
+        // ------------------------------------------------------------------------------------------------
+
+        public bool MoveNext()
+        {
+            if(posicion < elementsArray.Length - 1)
+            {
+                posicion++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            posicion = -1;
+        }
+
+
+        public object Current
+        {
+            get { return elementsArray[posicion]; }
+        }
+
+        // -------------------------------------------------------------------------------------------------
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this as IEnumerator;
+        }
+    }
+}
