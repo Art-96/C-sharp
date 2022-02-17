b2e0ead565d121773e401a914a1a223a2bc5a98f
Artur <arturgadyan123@gmail.com>
2022-02-17 14:49:09 +0400

    Conversion.

---
+++ OOP/016_Operators/04_Conversion/Explicit/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+// Overloading the explicit type conversion operator.
+
+namespace Explicit
+{
+    struct Digit
+    {
+        public byte value;
+
+        Digit(byte value)
+        {
+            this.value = value;
+        }
+
+        public static explicit operator Digit(byte argument)
+        {
+            Digit digit = new Digit(argument);
+            return digit;
+        }
+
+        public override string ToString()
+        {
+            return this.value.ToString();
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            byte variable = 1;
+
+            Digit digit = (Digit)variable;
+
+            Console.WriteLine(digit);
+        }
+    }
+}
---
+++ OOP/016_Operators/04_Conversion/Implicit/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+// Overloading the implicit type conversion operator.
+
+namespace Implicit
+{
+    struct Digit
+    {
+        public byte value;
+
+        Digit(byte value)
+        {
+            this.value = value;
+        }
+
+        public static implicit operator Digit(byte argument)
+        {
+            Digit digit = new Digit(argument);
+            return digit;
+        }
+
+        public override string ToString()
+        {
+            return this.value.ToString();
+        }
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+            byte variable = 1;
+
+            Digit digit = variable;
+            Console.WriteLine(digit);
+        }
+    }
+}
