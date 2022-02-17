3350efe014578cfe9d9acfb3c19ab4da26541562
Artur <arturgadyan123@gmail.com>
2022-02-17 12:51:02 +0400

    Deep copy.

---
+++ OOP/016_Operators/02_ICloneable/ICloneableWork/Point.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICloneableWork
+{
+    class Point : ICloneable
+    {
+        public int x, y;
+
+        public Point() { }
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public object Clone()
+        {
+            return new Point(this.x, this.y) as object;
+        }
+
+        public override string ToString()
+        {
+            return "X: " + x + " Y: " + y;
+        }
+    }
+}
---
+++ OOP/016_Operators/02_ICloneable/ICloneableWork/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ICloneableWork
+{
+    class Program
+    {
+        static void Main()
+        {
+            Point original = new Point(100, 100);
+            Point clone = original.Clone() as Point;
+
+            Console.WriteLine("First check.");
+
+            Console.WriteLine(original);
+            Console.WriteLine(clone);
+
+            clone.x = 0;
+
+            Console.WriteLine("Second check after change.");
+            Console.WriteLine(original);
+            Console.WriteLine(clone);
+        }
+    }
+}
